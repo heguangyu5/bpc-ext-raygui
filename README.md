fb87ad5b5fc21ad135e33eccb6c4ab34b2566589
heguangyu5 <heguangyu5@qq.com>
2024-05-29 16:38:12 +0800

    Container/separator controls ok

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# How to make
+# 1. How to make
 
 ```shell
 make debug && make && sudo make install
 bpc -v -c bpc-raygui.conf --static -d display_errors=on test.php
 ./test
 ```
+
+# 2. Data Structure Map
+
+| C                            | php                          |
+| ---------------------------- | ---------------------------- |
+| const char **text, int count | array('text1', 'text2', ...) |
 
--- php-raygui.scm
+++ php-raygui.scm
@@ -23,7 +23,13 @@
         ; Style set/get functions
         (guisetstyle control property value)
         (guigetstyle control property)
-        ; Controls
+        ; Container/separator controls, useful for controls organization
+        (guiwindowbox bounds title)
+        (guigroupbox bounds text)
+        (guiline bounds text)
+        (guipanel bounds text)
+        (guitabbar bounds tabs active)
+        (guiscrollpanel bounds text content scroll view)
         ; Basic controls set
         (guibutton bounds text)
         ; Advance controls set
@@ -87,7 +93,114 @@
                            ($belong->elong control)
                            ($belong->elong property)))))
 
-; Controls
+; Container/separator controls, useful for controls organization
+
+(define-macro (%with-c-bounds func-name . code)
+    `(begin
+        (pragma "Rectangle bounds")
+        (when (%init-c-rect ,func-name 1 bounds "bounds")
+            ,@code)))
+
+(define-macro (%with-c-text-list func-name arg-idx arg . code)
+    `(begin
+        (pragma "const char **text = NULL;
+                 int count = 0")
+        (unless (null? ,arg)
+            (set! ,arg (mkhashw ,func-name ,arg-idx ,arg))
+            (when ,arg
+                (let ((size (php-hash-size ,arg)))
+                    (when (>fx size 0)
+                        (pragma "text = (const char **)GC_MALLOC_ATOMIC(sizeof(char *) * $1)"
+                               ($bint->int size))
+                        (let loop ((t (php-hash-reset ,arg 'cont)))
+                            (when t
+                                (set! t (container-value t))
+                                (unless (string? t)
+                                    (set! t (or (mkstr t 'arg-parsing)
+                                                (php-error ,func-name "() expects parameter " ,arg-idx " to be array of strings, " (get-php-datatype t 'arg-parsing) " given"))))
+                                (when t
+                                    (pragma "text[count++] = $1" ($bstring->string t)))
+                                (loop (php-hash-advance ,arg 'cont))))
+                        (pragma "if (count == 0) {
+                                    text = NULL;
+                                 }")))))
+        ,@code))
+
+(defbuiltin (guiwindowbox bounds title)
+    (%with-c-bounds 'GuiWindowBox
+        (unless (string? title)
+            (set! title (mkstrw 'GuiWindowBox 2 title)))
+        (when title
+            (pragma::bool "GuiWindowBox(bounds, $1)" ($bstring->string title)))))
+
+(defbuiltin (guigroupbox bounds (text NULL))
+    (%with-c-bounds 'GuiGroupBox
+        (if (null? text)
+            (pragma::bool "GuiGroupBox(bounds, NULL)")
+            (begin
+                (unless (string? text)
+                    (set! text (mkstrw 'GuiGroupBox 2 text)))
+                (when text
+                    (pragma::bool "GuiGroupBox(bounds, $1)" ($bstring->string text)))))))
+
+(defbuiltin (guiline bounds (text NULL))
+    (%with-c-bounds 'GuiLine
+        (if (null? text)
+            (pragma::bool "GuiLine(bounds, NULL)")
+            (begin
+                (unless (string? text)
+                    (set! text (mkstrw 'GuiLine 2 text)))
+                (when text
+                    (pragma::bool "GuiLine(bounds, $1)" ($bstring->string text)))))))
+
+(defbuiltin (guipanel bounds (text NULL))
+    (%with-c-bounds 'GuiPanel
+        (if (null? text)
+            (pragma::bool "GuiPanel(bounds, NULL)")
+            (begin
+                (unless (string? text)
+                    (set! text (mkstrw 'GuiPanel 2 text)))
+                (when text
+                    (pragma::bool "GuiPanel(bounds, $1)" ($bstring->string text)))))))
+
+(defbuiltin (guitabbar bounds tabs (ref . active))
+    (%with-c-bounds 'GuiTabBar
+        (%with-c-text-list 'GuiTabBar 2 tabs
+            (let ((active-idx (container-value active)))
+                (unless (elong? active-idx)
+                    (set! active-idx (mkelongw 'GuiTabBar 3 active-idx)))
+                (when active-idx
+                    (pragma "int active")
+                    (pragma "active = (int)$1" ($belong->elong active-idx))
+                    (pragma "int result = GuiTabBar(bounds, text, count, &active)")
+                    (container-value-set! active (pragma::elong "active"))
+                    (pragma::elong "result"))))))
+
+(defbuiltin (guiscrollpanel bounds text content (ref . scroll) ((ref . view) unpassed))
+    (%with-c-bounds 'GuiScrollPanel
+        (unless (or (null? text)
+                    (string? text))
+                (set! text (mkstrw 'GuiScrollPanel 2 text)))
+        (when text
+            (pragma "Rectangle content")
+            (when (%init-c-rect 'GuiScrollPanel 3 content "content")
+                (pragma "Vector2 scroll = {0.0f, 0.0f}")
+                (when (let ((scroll-val (container-value scroll)))
+                        (or (null? scroll-val)
+                            (%init-c-vector2 'GuiScrollPanel 4 scroll-val "scroll")))
+                    (pragma "Rectangle view = {0}")
+                    (when (or (eq? view unpassed)
+                              (let ((view-val (container-value view)))
+                                (or (null? view-val)
+                                    (%init-c-rect 'GuiScrollPanel 5 view-val "view"))))
+                        (pragma "int result")
+                        (pragma "result = GuiScrollPanel(bounds, STRINGP($1) ? BSTRING_TO_STRING($1) : NULL, content, &scroll, &view)"
+                                text)
+                        (unless (eq? scroll unpassed)
+                            (container-value-set! scroll (%mkvector2-v "scroll")))
+                        (unless (eq? view unpassed)
+                            (container-value-set! view (%mkrect-r "view")))
+                        (pragma::bool "result")))))))
 
 ; Basic controls set
 
